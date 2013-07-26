6bd6bb194026fd74e4ffb4a20aa7d29e0a5b51b9
Alex <end.user@gmail.com>
2013-07-26 00:18:43 -0700

    intial add

---
+++ restaurants.py
@@ -0,0 +1,25 @@
+import json
+
+class restaurants:
+    def __init__(self):
+        self.restaurants = {}
+    def dumpDataFile(self, filename):
+        # heh, he said dump
+        # will always overwrite
+        fh = open('./%s' % filename, 'w')
+        fh.write(json.dumps(self.restaurants))
+        fh.close()
+    def loadDataFile(self, filename):
+        # heh, he said load
+        fh = open('./%s' % filename, 'r')
+        output = fh.readlines()[0]
+        fh.close()
+        print output
+        self.restaurants = json.loads(output)
+    def add(self, name, location, flags):
+        # name = string
+        # location = string
+        # flags = list
+        self.restaurants[name] = {} 
+        self.restaurants[name]['location'] = location
+        self.restaurants[name]['flags'] = flags
