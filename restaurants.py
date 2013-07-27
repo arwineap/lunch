dc15ecfc30a566ccafef40a99466e54eef9b20b6
Alex <end.user@gmail.com>
2013-07-26 18:51:42 -0700

    Fixed stupid bug

--- restaurants.py
+++ restaurants.py
@@ -15,10 +15,29 @@ class restaurants:
         output = fh.readlines()[0]
         fh.close()
         self.restaurants = json.loads(output)
-    def add(self, name, location, flags=None):
+    def add(self, name, location, flags=None, nick=None):
         # name = string
         # location = string
         # flags = list
         self.restaurants[name] = {} 
         self.restaurants[name]['location'] = location
         self.restaurants[name]['flags'] = flags
+        self.restaurants[name]['nick'] = nick
+    def mod(self, name, location=None, flags=None, nick=None):
+        # Note: flags is a tuple here (['potential', 'new', 'flags'], 'a')
+        #                             (['potential', 'new', 'flags'], 'w')
+        if name in self.restaurants:
+            if location:
+                self.restaurants[name]['location'] = location
+            if nick:
+                self.restaurants[name]['nick'] = nick
+            if flags:
+                if flags[1] == 'a':
+                    # append
+                    self.restaurants[name]['flags'] = self.restaurants[name]['flags'] + flags[0]
+                elif flags[1] == 'w':
+                    self.restaurants[name]['flags'] = flags[0]
+                else:
+                    raise TypeError("flags is not in correct format")
+        else: 
+            raise IndexError("%s does not exist" % name)
